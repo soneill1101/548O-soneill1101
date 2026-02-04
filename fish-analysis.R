ca165249e091fe3ffd1b2bf7fca39bbc4ae0eed5
unknown <steffani.oneill@ubc.ca>
2026-02-04 15:13:20 -0800

    Move data file into subdirectory

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,4 +1,15 @@
+
+# Read data in new script
+# Label the Gaeta.cvs data as "fish_data"
+fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+
 # Create new categorical size column
 library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+
+# Change cateogry cut-off size
+fish_data_cat = fish_data %>% 
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+
